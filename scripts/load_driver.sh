358d5ec0870810ae06c2c906adb639133859ef91
pujit098 <pujithcheeli@gmail.com>
2025-12-17 17:39:40 +0530

    Fix Jenkins pipeline: remove sudo, correct script paths

--- scripts/load_driver.sh
+++ scripts/load_driver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo insmod driver/i2c_dummy_driver.ko
+insmod driver/i2c_dummy_driver.ko
 chmod +x scripts/load_driver.sh
 
--- scripts/unload_driver.sh
+++ scripts/unload_driver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo rmmod i2c_dummy_driver
+rmmod i2c_dummy_driver
 chmod +x scripts/unload_driver.sh
 
